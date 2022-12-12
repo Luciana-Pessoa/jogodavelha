6708ced357bfff6a7f617926afde8f99f1ffdf8b
Luciana <lu992323258@gmail.com>
2022-12-12 20:41:21 -0300

    js

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,14 @@
-//variaveis
-
-let board = [",",",",",",",","];
+let board = [",", ",", ",", ",", "];
 let playerTime = 0;
-let symbols = ['o','x'];
+let symbols = ['o', 'x'];
+
+function handleMove(position) {
+    board[position] = symbols[playerTime];
+
+    if (playerTime == 0) {
+        playerTime = 1;
+    } else {
+        playerTime = 0;
+
+    }
+}
